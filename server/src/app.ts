7ed90cb0382914fe360781cfd26b591d19b1a987
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2025-03-24 22:09:55 +0100

    fix: change default port

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,19 @@
 import express, { NextFunction, Request, Response, Router } from "express";
 import cors from "cors";
 import "dotenv/config";
-import "module-alias/register"
+import "module-alias/register";
 import connectDb from "./config/db";
 import {
-  authRoutes, userRoutes, postRoutes,
-  commentRoutes, likeRoutes, followRoutes,
-  searchRouters
+  authRoutes,
+  userRoutes,
+  postRoutes,
+  commentRoutes,
+  likeRoutes,
+  followRoutes,
+  searchRouters,
 } from "./routes";
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 
 connectDb();
 const apiRouter: Router = express.Router();
-
 
 apiRouter.use("/auth", authRoutes);
 apiRouter.use("/user", userRoutes);
@@ -29,9 +32,8 @@ apiRouter.use("/search", searchRouters);
 
 app.use("/api", apiRouter);
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.status(404).json({ message: "Page Not Found" })
-
-})
+  res.status(404).json({ message: "Page Not Found" });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
